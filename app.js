2994ec72757f14ed16ac65419ddaa329ccb728f3
gitsammy09 <sammyfooty09@gmail.com>
2023-03-23 14:44:30 +1100

    Connecting to Mongo DB

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+//Moduels
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const CampGround = require('./models/campground');
+
+//Database Connection
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => {
+        console.log('Mongo Database Connected');
+    })
+    .catch(err => {
+        console.log('Connection Error!!!');
+        console.log(err);
+    })
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -8,6 +22,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/',(req,res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async (req,res) => {
+    const camp = new CampGround({title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000, () => {
