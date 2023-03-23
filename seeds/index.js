82940b351777aac30bb38176f5fe2264291db8e0
gitsammy09 <sammyfooty09@gmail.com>
2023-03-24 00:54:58 +1100

    Adding-images

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
         await CampGround.deleteMany({});
         for(let i = 0; i < 50; i++){
             const random1000 = Math.floor(Math.random() * 1000);
+            const price = Math.floor(Math.random() * 30) + 10
             const camp = new CampGround({
                 location: `${cities[random1000].city}, ${cities[random1000].state}`,
-                title: `${sample(descriptors)} ${sample(places)}`
+                title: `${sample(descriptors)} ${sample(places)}`,
+                image: 'https://source.unsplash.com/collection/483251',
+                description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia quos perspiciatis est repellendus, et facilis tenetur dolorem quaerat unde nostrum, facere assumenda eligendi. Nihil in, voluptate excepturi adipisci sit voluptatibus!',
+                price
             })
             await camp.save();
         }    
