dc18b33446b2d77ab541ac6d3f69afd76090947e
gitsammy09 <sammyfooty09@gmail.com>
2023-03-23 23:52:47 +1100

    Adding-Layouts-with-ejsmate-package

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -18,10 +19,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 const app = express();
 
 //Middlewares
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+
 
 
 app.get('/',(req,res) => {
