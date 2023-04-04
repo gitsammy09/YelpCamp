58d62a86f1f4bfaea9138d4db2773edd8e3b0870
gitsammy09 <sammyfooty09@gmail.com>
2023-04-04 16:15:56 +1000

    Adding-thubnail-virtual-property

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema  = mongoose.Schema;
 
+//https://res.cloudinary.com/demo/image/upload/c_fill,h_300,w_250/e_blur:300/sample.jpg
+
+const ImageSchema = new Schema({
+     url: String,
+    filename: String     
+});
+
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload', '/upload/w_200');
+})
 const CampGroundSchema = new Schema({
     title: String,
-    images: [
-        {
-          url: String,
-          filename: String  
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
